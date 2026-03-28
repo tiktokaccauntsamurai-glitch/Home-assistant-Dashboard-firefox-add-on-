a06b459c3cd6eb894e39e1cb6ce4c90a260818d1
Monserrat <tiktokaccauntsamurai@gmail.com>
2026-03-28 19:32:05 +0300

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-// Redirects about:home (Firefox homepage) to the custom new tab page
-browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url === 'about:home' || tab.url === 'about:newtab') {
-    browser.tabs.update(tabId, {
-      url: browser.runtime.getURL('newtab.html')
-    });
-  }
-});
